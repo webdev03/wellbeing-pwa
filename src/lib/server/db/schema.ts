6a08d56701c20845fdc95e89e0f7b2b9c3b4f23b
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-11-19 22:16:18 +1300

    format

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,6 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
 
-export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+export const user = pgTable("user", {
+  id: serial("id").primaryKey(),
+  age: integer("age")
 });
