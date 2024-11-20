f0512665cf46cbe57eae4f932d69bbf04b50aac1
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-11-20 20:52:16 +1300

    base

---
+++ src/routes/(home)/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import "../app.css";
+  let { children } = $props();
+  interface LinkBarItem {
+    url: string;
+    text: String;
+  }
+
+  const links: LinkBarItem[] = [
+    {
+      url: "/app",
+      text: "App"
+    },
+    {
+      url: "/policy",
+      text: "Legal Policy"
+    }
+  ];
+</script>
+
+<svelte:head>
+  <title>Wellbeing</title>
+  <meta name="description" content="An app that allows users to manage their wellbeing.">
+</svelte:head>
+
+<nav class="border-gray-200 bg-white dark:bg-gray-900">
+  <div class="mx-auto flex flex-wrap items-center justify-between p-4">
+    <a href="/" class="flex items-center self-center text-center text-xl font-semibold">Wellbeing</a
+    >
+    <div class="w-auto">
+      <ul class="flex flex-row font-medium">
+        {#each links as link}
+          <li>
+            <a
+              href={link.url}
+              aria-current={$page.url.pathname.startsWith(link.url) ? "page" : undefined}
+              class={`rounded px-3 py-2 transition-all hover:bg-gray-200 ${$page.url.pathname.startsWith(link.url) ? "text-blue-600" : ""}`}
+              >{link.text}</a
+            >
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</nav>
+{@render children()}
---
+++ src/routes/(home)/+page.svelte
@@ -0,0 +1,12 @@
+<section
+  class="mx-auto flex w-full flex-col items-center gap-2 p-2 py-6 pb-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20"
+>
+  <h1
+    class="text-center text-4xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]"
+  >
+    Wellbeing
+  </h1>
+  <p class="max-w-[750px] text-balance text-center text-lg text-muted-foreground sm:text-xl">
+    Learn and have fun with activities related to mathematics.
+  </p>
+</section>
